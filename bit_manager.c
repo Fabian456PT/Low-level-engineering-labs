b5c443d88b4a1830433ba312b9688ad0a8f4a8ad
flaviodoano <fabian.neves.rodrigues@gmail.com>
2026-03-15 18:52:29 +0000

    Base structure finished

--- bit_manager.c
+++ bit_manager.c
@@ -1,28 +1,69 @@
 #include <stdio.h> 
 
 int main(){
-    unsigned char dados = 0, mascara = 1, bit = 0;
-    
+    unsigned char data = 0, mask = 1, bit = 0, instruction = 0;
+
     printf("Olá ziqui,\nDiz um número: ");
-    scanf("%hhu", &dados);
+    scanf("%hhu", &data);
 
-    printf("Qual é o bit que queres ver?: ");
-    scanf("%hhu", &bit);
+    printf("Qual é a operaçao que queres fazer?:\n"
+    "_________________________________________\n"
+    "\n1 - verify\n"
+    "2 - Turn On\n"
+    "3 - Turn off\n"
+    "_________________________________________\n");
 
+    scanf("%hhu", &instruction);
 
-    mascara = mascara<<(bit-1);
+    if (instruction == 1) {
+     
+        printf("Qual é o bit que queres ver?: \n");
+        scanf("%hhu", &bit);
+        mask = mask<<bit;
     
-    dados = mascara&dados;
+        if ((mask & data&mask) == mask) {
+            printf("o bit %dº está ligado", bit);
+            return 0;
+        } 
+        else {
+            printf("o bit %dº está desligado", bit);      
+            return 0;  
+        }
 
-    if ((mascara & dados) == mascara) {
-        printf("o bit %dº está ligado", bit);
-        return 0;
-    } 
-    else {
-        printf("o bit %dº está desligado", bit);      
-        return 0;  
     }
+
+
+    if (instruction == 2) {
+        printf("What's the bit that you want to turn on?: \n");
+        scanf("%hhu", &bit);
+        
+        mask = mask<<bit;
     
+        if ((data & mask) == mask) {
+            printf("The bit is already turned on.\n");
+            return 0;
+        }
+
+        data = data | mask;    
+
+        printf("The bit is now turned on.\n");            
+        
+    }    
+
+    if (instruction == 3) {
+        printf("What's the bit that you want to turn off?: \n");                
+        scanf("%hhu", &bit);
+
+        mask = mask<<bit;
+
+        if ((data & mask) == 0) {
+           printf("The bit is already turned off.\n");
+           return 0;
+        }
+        data = ~mask & data;
+
+        printf("The bit is now turned off.\n");
+    }
 
     return 0;
 
