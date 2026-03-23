f4b4dae572ec90654ebd6accd195b19cc546fbc6
flaviodoano <fabian.neves.rodrigues@gmail.com>
2026-03-23 12:23:13 +0000

    feat: implement permissions simulator with octal validation

--- 02_linux_permissions/linux_permissions.c
+++ 02_linux_permissions/linux_permissions.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-void cmd_process(unsigned char user, unsigned char group, unsigned char others){
-    char buffer[9];
+void print_command(unsigned char digit, char *buffer){
     unsigned char mask = 1;
 
-    if ((user & mask<<2) == 4) {
+    if ((digit & 4) == 4) {
         buffer[0] = 'r';
     }
-    else if ((user & mask<<1) == 2) {
+    else {
+        buffer[0] = '-';        
+    }
+
+    if ((digit & 2) == 2) {
         buffer[1] = 'w';
     }
-    else if ((user & mask<<1) == 1) {
+    else {
+        buffer[1] = '-';
+    }
+
+    if ((digit & 1) == 1) {
         buffer[2] = 'x';    
+    }
+    else {
+        buffer[2] = '-';        
     }
 }
 
@@ -20,32 +30,37 @@ int main(){
     unsigned char user = 0, group = 0, others = 0;
 
     int number;
+    char buffer[10] = "\0";
+
     printf("Choose a number with 3 digits (0-7): ");
     scanf("%o", &number);
 
+    //defining the user digit
     user = number >> 6;
-        if ((user & 7) >= 0){
-        printf("The 1st bit is greater than 7");
+    if (user > 7) {
+        printf("Number out of range\n");
+        return 0;
     }
 
-    //defining the group comand
-    group = number;
-    group <<= 3;
-    group >>= 6;
-    if (group > 7 && group >= 0){
-        printf("The 2nd bit is greater than 7");
+    //defining the group digit
+    group = (number >> 3) & 7;
+    if (group > 7) {
+        printf("Number out of range\n");
+        return 0;
     }
 
-    //defining the others comand
-    others = number;
-    others <<= 6;   
-    others >>= 6;
-    if (others > 7 && others >= 0){
-        printf("The 3rd bit is greater than 7");
+    //defining the others digit
+    others = number & 7;
+    if (others > 7) {
+        printf("Number out of range\n");
+        return 0;
     }
 
-    cmd_process(user,group,others);
+    print_command(user, &buffer[0]);
+    print_command(group, &buffer[3]);
+    print_command(others, &buffer[6]);
 
+    printf("%s\n", buffer);
 
     return 0;
 }
